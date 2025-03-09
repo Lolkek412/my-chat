4c5bde88a3214535d23dafee699f6aa0dffb77b3
Lolkek412 <ffffffff1112333@gmail.com>
2025-03-09 17:09:38 +0900

    server.js

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ wss.on("connection", (ws) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log("Сервер запущен на http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
 });
